ceef290efe26b22ec28db036ea9941b26c401207
Jeremy-Bergese <jeremybergese@gmail.com>
2023-02-15 16:15:38 +0100

    First commit

---
+++ src/main/java/dev/JeremyBergese/JustAnotherMoviesWebApp/JustAnotherMoviesWebAppApplication.java
@@ -0,0 +1,13 @@
+package dev.JeremyBergese.JustAnotherMoviesWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAnotherMoviesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAnotherMoviesWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/JeremyBergese/JustAnotherMoviesWebApp/JustAnotherMoviesWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.JeremyBergese.JustAnotherMoviesWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAnotherMoviesWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
